043206d69999c542795fee1818b2696b4417124c
Leonid <lleonidk@ukr.net>
2025-09-17 00:25:39 +0300

    add titles

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,29 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
+import {useEffect} from "react";
 
-import "./Layout.css"
+import "./Layout.css";
 import Header from "./components/header/Header.tsx";
 
 function Layout() {
+    const location = useLocation();
+
+    useEffect(() => {
+        const titles: Record<string, string> = {
+            "/": "Главная",
+            "/contacts": "Контакты",
+            "/menu1": "1200 - Slim Control",
+            "/menu2": "1600 - Slim Fit",
+            "/menu3": "1800 - Fit Balance",
+            "/menu4": "2200 - Balance Energy",
+            "/menu5": "2500 - Active Energy",
+            "/menu6": "3000 - Muscle Gain",
+            "/menu7": "3200 - Mass Builder",
+            "/menu8": "Преміум меню",
+        };
+
+        document.title = "Food | " + titles[location.pathname] || "Мій сайт";
+    }, [location]);
+
     return (
         <div className="page">
             <Header/>
@@ -15,4 +35,4 @@ function Layout() {
     );
 }
 
-export default Layout
+export default Layout;
--- src/components/pages/menus/Menu.tsx
+++ src/components/pages/menus/Menu.tsx
@@ -3,14 +3,16 @@ import WeekDay from "./WeekDay.tsx";
 import type {Day} from "../../../data/weekData.ts";
 
 interface MenuProps {
+    type: string
     weekData: Day[];
 }
 
-function Menu({ weekData }: MenuProps) {
+function Menu({type, weekData}: MenuProps) {
     return (
         <div className="week__container">
+            <h1>{type}</h1>
             {weekData.map((day, idx) => (
-                <WeekDay key={idx} day={day.day} meals={day.meals} />
+                <WeekDay key={idx} day={day.day} meals={day.meals}/>
             ))}
         </div>
     );
